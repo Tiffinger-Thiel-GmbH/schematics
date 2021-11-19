16d533db4ff28f9c4bd6e69ff8734be3f1ca11ae
johoermann <johannes.hoermann@tiffinger-thiel.de>
2021-11-19 10:18:48 +0100

    Use tiffinger-thiel/eslint-config

--- src/lib/application/files/ts/.eslintrc.js
+++ src/lib/application/files/ts/.eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    "standard-with-typescript",
-
-    // Has to be the last:
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js', '.prettierrc.js'],
-  rules: {},
+  extends: ['@tiffinger-thiel/eslint-config/profile/nest'],
 };
